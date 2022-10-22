638f811b1af88c8d4090cd420968b27c3f6e8ef9
JohnnLoR <johnnyradamanthis@gmail.com>
2022-10-22 02:39:31 -0500

    DEV_JohnnLoR - First commit include example and Proyect MinTIC2022_UNAL Cicle4a

---
+++ FlaskMongoEjemplo/Repositorios/PartidoRepositorio.py
@@ -0,0 +1,6 @@
+from Modelos.AbstractModel import AbstractModel
+from Repositorios.InterfazRepositorio import InterfazRepositorio
+from Modelos.Partido import Partido
+
+class PartidoRepositorio(InterfazRepositorio[Partido]):
+    pass
