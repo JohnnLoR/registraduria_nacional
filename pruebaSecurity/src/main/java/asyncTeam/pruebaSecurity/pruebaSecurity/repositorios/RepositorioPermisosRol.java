5a38a476e08c047032c0cbb998846c6175127fa5
Johnny Loaiza Ruiz <115382962+JohnnLoR@users.noreply.github.com>
2022-11-03 22:41:08 -0500

    DEV_JohnnLoR Modelo-Repositorio-Controlador PermisosRoles

---
+++ pruebaSecurity/src/main/java/asyncTeam/pruebaSecurity/pruebaSecurity/repositorios/RepositorioPermisosRol.java
@@ -0,0 +1,10 @@
+package asyncTeam.pruebaSecurity.pruebaSecurity.repositorios;
+
+import asyncTeam.pruebaSecurity.pruebaSecurity.modelos.PermisosRol;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RepositorioPermisosRol extends MongoRepository<PermisosRol, String> {
+    @Query("{'rol.$id': ObjectId(?0), 'permiso.$id': ObjectId(?1)}")
+    PermisosRol getPermisoRol(String id_rol, String id_permiso);
+}
