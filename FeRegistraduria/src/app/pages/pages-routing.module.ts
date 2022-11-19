53ea3a3119bd21f01d7a26cf3ab1409fbb4e011d
Johnny Loaiza Ruiz <115382962+JohnnLoR@users.noreply.github.com>
2022-11-18 22:19:29 -0500

    DEV_JohnnLoR Configuración Inicial FrontEnd, Componente/Package Seguridad-Login y Mesas

--- FeRegistraduria/src/app/pages/pages-routing.module.ts
+++ FeRegistraduria/src/app/pages/pages-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [{
       loadChildren: () => import('./seguridad/seguridad.module').then(m => m.SeguridadModule),
     },
     {
+      path: 'mesas',
+      loadChildren: () => import('./mesas/mesas.module').then(m => m.MesasModule),
+    },
+    {
       path: 'dashboard',
       component: ECommerceComponent,
     },
